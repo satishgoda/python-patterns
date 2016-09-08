a17cadbab18578ef6bca9876ced7bf62ffe96150
Satish Goda <satishgoda@live.com>
2016-09-09 05:55:00 +0800

    Meet the abstract Builder
    The Builder will specify the building operations that will create the representation. The Builder will not specify the order of the operations!

--- builder/remix_builder.py
+++ builder/remix_builder.py
@@ -9,3 +9,16 @@ class Building(Product):
     
     def __repr__(self):
         return "Building/{0.type}\n\tFloor: {0.floor}\n\tSize: {0.size}".format(self)
+
+class Builder(object):
+    def __init__(self):
+        self.building = None
+    
+    def new_building(self):
+        self.building = Building(self.type)
+    
+    def build_floor(self):
+        raise NotImplementedError
+    
+    def build_size(self):
+        raise NotImplementedError
